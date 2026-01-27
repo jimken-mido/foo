9d892b127434e1616904085cb1d3c0e817132846
JimKennedy <jim@midokura.com>
2026-01-27 16:35:29 +0100

    blogSidebarCount: 'ALL',

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,6 +55,7 @@ const config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarCount: 'ALL',
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
